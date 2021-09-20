5c8c5684e1adf1e16cd2861c94f445916b0b8cbf
Igor Zhigulin <igor.iz.zhigulin@gmail.com>
2021-09-20 22:29:07 +0700

    Доработка скрипта сборки

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
-pdflatex --shell-escape main.tex
+pdflatex -synctex=1 -interaction=nonstopmode --shell-escape main.tex
+pdflatex -synctex=1 -interaction=nonstopmode --shell-escape main.tex
 rm *.aux *.log *.out *.toc 
 evince main.pdf
