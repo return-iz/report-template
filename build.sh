ee7609b27ea692c9c66bdfc5533e3f8929aaa3f3
Igor Zhigulin <igor.iz.zhigulin@gmail.com>
2021-10-27 03:15:35 +0700

    pdflatex -> xelatex. Потому что выбор шрифтов есть, который я и добавил

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-pdflatex -synctex=1 -interaction=nonstopmode --shell-escape main.tex
-pdflatex -synctex=1 -interaction=nonstopmode --shell-escape main.tex
+xelatex -synctex=1 -interaction=nonstopmode --shell-escape main.tex
+xelatex -synctex=1 -interaction=nonstopmode --shell-escape main.tex
 rm *.aux *.log *.out *.toc 
 evince main.pdf
